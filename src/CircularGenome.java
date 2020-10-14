1aca9e2003e399dc66a330d724252cbf9042623a
Dan DeBlasio <dan@dandeblasio.com>
2020-10-14 15:31:30 -0600

    Comments

--- src/CircularGenome.java
+++ src/CircularGenome.java
@@ -1,8 +1,11 @@
 
 public class CircularGenome {
+  // two Nucleotide pointers for head and tail
   Nucleotide head;
   Nucleotide tail;
   
+  // constructor populates the head and tail pointer with Nucleotides
+  // should use charAt and toUpperCase
   CircularGenome(String inGenome){
     String allCapsGenome = inGenome.toUpperCase();
     head = new Nucleotide(allCapsGenome.charAt(0));
@@ -14,6 +17,8 @@ public class CircularGenome {
     tail.next = head;
   }
   
+  // output a linearized version of the circular genome, use the pointers to know when youve made one rotation
+  // no need to change case here, will always output in all caps
   public String toString() {
     Nucleotide current = head;
     String rtn = "";
@@ -25,6 +30,13 @@ public class CircularGenome {
     return rtn;
   }
   
+  /* checks each position as a possible start location either returns:
+   *    "'<all caps search>' found at position(s) #, #...." 
+   *         or 
+   *   "'<all caps search>' not found"
+   * 
+   *    should use toUpperCase and match but no need for charAt in this method
+   */
   public String search(String inSequence) {
     String allCapsSequence = inSequence.toUpperCase();
     String foundString = "";
--- src/Nucleotide.java
+++ src/Nucleotide.java
@@ -1,21 +1,33 @@
 
 public class Nucleotide {
+  // Class variables: (private char, public Nucleotide)
   private char base; 
   public Nucleotide next;
   
+  // Constructor
   Nucleotide(char inBase){
     base = inBase;
   }
   
+  // Getter
   public char getChar() {
     return base;
   }
   
+  // recursive match
+  //   given a string match the first char if possible, then recursively call on rest of list
   public boolean match(String inSequence) {
+    
+    // if not equal return false
     if(inSequence.charAt(0) == base) {
+      
+      // if this is the last thing to check, found the string
       if(inSequence.length() <= 1) return true;
+      
+      // if more string, trim off front then recursive call
       return next.match(inSequence.substring(1));
     }
+    
     return false;
   }
 }
